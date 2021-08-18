fbfb08152fdb6f1a49c9cb97fe2edf35b0319938
Emilie Tole <eetole@gmail.com>
2021-08-18 14:17:21 -0400

    started shuffle functions

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-let startButton = document.getElementById ("startButton").addEventListener("click", shuffleDeck);
+let startButton = document
+  .getElementById("startButton")
+  .addEventListener("click", shuffleDeck);
 let newDeck = [];
 let cardDeck = document.querySelector("cardDeck");
+let timer = document.querySelector("timer");
+let seconds;
 
-
+//shuffle deck and re-position cards
 const shuffle = (array) => {
-    let currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-  
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  };
-  
-const shuffleDeck = () => {
-    shuffle(newDeck);
-    cardDeck.innerHTML = "";
-    for (let item of newDeck) {
-      cardDeck.append(item);
-    }
-  };
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
+
+const shuffleDeck = () => {
+  shuffle(newDeck);
+  cardDeck.innerHTML = "";
+  for (let item of newDeck) {
+    cardDeck.append(item);
+  }
+};
+
+// manipulate html text
+const startTimer = () => {
+  myTimer = setInterval(() => {
+    if (seconds === 0) {
+      clearInterval(myTimer);
+      matchCounter = 0;
+    } else {
+      seconds--;
+      timer.innerText = `Countown: ${seconds} seconds`;
+    }
+  }, 1000);
+};
